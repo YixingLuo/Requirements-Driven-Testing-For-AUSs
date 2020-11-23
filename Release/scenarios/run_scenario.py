aec86f8283bae1dcc924c56c7183f6ca361c0676
Yixing Luo <yixingluo@pku.edu.cn>
2020-11-23 12:37:21 +0800

    1123

--- Release/scenarios/run_scenario.py
+++ Release/scenarios/run_scenario.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 time = 10
 num_sce = 1
-file_path = r"C:\Users\lenovo\Documents\GitHub\mazda-path-planner-sbt_changes\mazda-path-planner-sbt_changes\ERATO_planning\\x64\Release\\"
+file_path = r"C:/Users/lenovo/Documents/GitHub/Requirements-Driven-Testing-For-AUSs/ReleaseRelease\\"
 scenario_name = file_path + "scenarios\scenario_" + str(num_sce) + ".json"
 log_name = file_path + "datalog\datalog_" + str(num_sce) + ".txt"
 cmd = file_path + "dynamic_cost -c %d -v EGO_TESTER -a -i %s > %s" % (time, scenario_name, log_name)
