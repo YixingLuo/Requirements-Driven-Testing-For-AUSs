a4ebd2407c560323c41d2f5c9dc1af2a8ea144ed
Yixing Luo <yixingluo@pku.edu.cn>
2020-12-25 23:59:12 +0800

    1225

--- UUV/UUV_BruteForce/output_transform.m
+++
@@ -1,9 +0,0 @@
-
-iternum = 100;
-duration = 360;
-output_data = [];
-for i = 1 : 15*iternum
-    output_data(i) = (1-data1(i,1))*2^0 + (1-data1(i,3))*2^1 + (1-data1(i,5))*2^2;
-end
-output_data
-output_data = categorical(output_data)
