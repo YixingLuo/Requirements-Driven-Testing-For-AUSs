a3636b3fd3589b65417ccc79663ed7654f9fe1ff
Yixing Luo <yixingluo@pku.edu.cn>
2021-01-03 15:24:44 +0800

    0103

--- Req_SBT/Random_Search/Random_Overtake.sh
+++ Req_SBT/Random_Search/Random_Overtake.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -J random_overtake
 #SBATCH -o random_overtake.%j.%N.out
-#SBATCH --partition=C144M4096G
+#SBATCH --partition=C032M0256G
 #SBATCH --qos=high
 #SBATCH -A hpc0006178148
 #SBATCH --get-user-env
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=50
+#SBATCH --ntasks-per-node=32
 #SBATCH --mail-type=end
 #SBATCH --mail-user=yixingluo@pku.edu.cn
 #SBATCH --time=120:00:00
