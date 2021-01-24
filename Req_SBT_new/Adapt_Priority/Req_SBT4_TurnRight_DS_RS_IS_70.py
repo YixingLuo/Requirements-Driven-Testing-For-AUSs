fee7f4d10c54609b39bca1f0bd8d8b8219412513
Yixing Luo <yixingluo@pku.edu.cn>
2021-01-24 17:45:08 +0800

    0124

--- Req_SBT_new/Adapt_Priority/Req_SBT4_TurnRight_DS_RS_IS_70.py
+++ Req_SBT_new/Adapt_Priority/Req_SBT4_TurnRight_DS_RS_IS_70.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         # search_round_list = [1, 10, 10, 10, 10, 20, 110, 110]
         # search_round_list = [1, 10, 20, 30, 40, 50, 60, 70]
         search_round_list = [70, 70, 70, 70, 70, 70, 70, 70]
-        target_value_threshold = [-1/5.0, 0, -16.67, 1, 0, -0.01, -0.2]
+        target_value_threshold = [-1 / 5.0, 0, -16.67, 1, 0, -0.1, -0.4]
         target_dir = data_folder
 
 
